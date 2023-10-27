16d0c3ade938665af82449767616d184f045ccdf
troferoller <90683935+troferoller@users.noreply.github.com>
2023-10-27 18:26:11 +0300

    Update TestBoxText.java
    edded println "Nothing"

--- src/test/java/tests/TestBoxText.java
+++ src/test/java/tests/TestBoxText.java
@@ -15,5 +15,6 @@ public class TestBoxText {
         open("https://www.google.com");
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://ru.selenide.org"));
+        System.out.println("nothing");
     }
 }
